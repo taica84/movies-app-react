79479a99ee9b5c3237ba972031a3dfdfad3defc6
taica84 <taica_rules@yahoo.com>
2021-05-14 19:47:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [movies , setMovies] = useState([
+
+        {
+            "Title": "The Avengers",
+            "Year": "2012",
+            "imdbID": "tt0848228",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Avengers: Endgame",
+            "Year": "2019",
+            "imdbID": "tt4154796",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Avengers: Age of Ultron",
+            "Year": "2015",
+            "imdbID": "tt2395427",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Avengers",
+            "Year": "1998",
+            "imdbID": "tt0118661",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BYWE1NTdjOWQtYTQ2Ny00Nzc5LWExYzMtNmRlOThmOTE2N2I4XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Avengers: Earth's Mightiest Heroes",
+            "Year": "2010–2012",
+            "imdbID": "tt1626038",
+            "Type": "series",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BYzA4ZjVhYzctZmI0NC00ZmIxLWFmYTgtOGIxMDYxODhmMGQ2XkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Ultimate Avengers: The Movie",
+            "Year": "2006",
+            "imdbID": "tt0491703",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMTYyMjk0NTMwMl5BMl5BanBnXkFtZTgwNzY0NjAwNzE@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Ultimate Avengers II",
+            "Year": "2006",
+            "imdbID": "tt0803093",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BZjI3MTI5ZTYtZmNmNy00OGZmLTlhNWMtNjZiYmYzNDhlOGRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Avengers",
+            "Year": "1961–1969",
+            "imdbID": "tt0054518",
+            "Type": "series",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BZWQwZTdjMDUtNTY1YS00MDI0LWFkNjYtZDA4MDdmZjdlMDRlXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Avengers Assemble",
+            "Year": "2012–2019",
+            "imdbID": "tt2455546",
+            "Type": "series",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMTY0NTUyMDQwOV5BMl5BanBnXkFtZTgwNjAwMTA0MDE@._V1_SX300.jpg"
+        }
+    ]);
+    const [searchValue , setSearchValue] = useState('');
+
+    const getMovieRequest = async () => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=57c2f23e`
+
+        const response = await fetch(url);
+        const responseJson = await response.json();
+        console.log(responseJson);
+    
+        if(responseJson.Search) {
+            setMovies(responseJson.Search)
+        }
+       
+    }
+
+      useEffect(()=> {
+          getMovieRequest(searchValue);
+      },[searchValue]);
+
+    return (
+        <div className='container-fluid movie-app' >
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+              <MovieListHeading  heading='Movies' />
+              <SearchBox 
+              searchValue={searchValue} setSearchValue={setSearchValue}
+              />
+            </div>
+
+            <div className='row' >
+               <MovieList movies={movies} />
+              
+            </div>
+           
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/MovieList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import uuid from 'react-uuid';
+
+const MovieList = (props) => {
+      
+  
+
+    return (
+        <>
+           {props.movies.map((movie , index) => 
+            
+             <div className='image-container d-flex justify-content-start m-3' key={uuid()}>
+                   <img 
+                   src={movie.Poster} alt='movie'
+               
+                   />
+                   <div className='overlay d-flex align-items-center justify-content-center'>
+
+                   </div>
+               </div>
+          )} 
+        </>
+    )
+}
+
+export default MovieList;
